df98feb5ab6a023ef76e73c395ab98f9351f8336
nguyentamtgdd2005-ui <nguyentamtgdd2005@gmail.com>
2025-11-10 09:26:38 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "tan.mysql.database.azure.com";
+$configDB["host"] 		= "tan-prod.mysql.database.azure.com";
 $configDB["database"]	= "computer_store";
-$configDB["username"] 	= "Trongtan";
+$configDB["username"] 	= "tanadmin";
 $configDB["password"] 	= "12345678@a";
-define("HOST", "tan.mysql.database.azure.com");
+define("HOST", "tan-prod.mysql.database.azure.com");
 define("DB_NAME", "computer_store");
-define("DB_USER", "Trongtan");
+define("DB_USER", "tanadmin");
 define("DB_PASS", "12345678@a");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
